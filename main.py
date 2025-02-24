5238104f9bc087794c6aac69a87df1adda8b872b
izartorius <atreuseinzbelt@example.com>
2025-02-24 13:04:39 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2
+import mediapipe as mp
+cam = cv2.VideoCapture(0)
+face_mesh = mp.solutions.face_mesh.FaceMesh()
+while True:
+    _, frame = cam.read()
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = face_mesh.process(rgb_frame)
+    landmark_points = output.multi_face_landmarks
+    frame_h, frame_w, _ = frame.shape
+    if landmark_points:
+        landmarks = landmark_points[0].landmark
+        for landmark in landmarks:
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            print(x, y)
+    cv2.imshow('Eye Controlled Mouse', frame)
+    cv2.waitKey(1)
